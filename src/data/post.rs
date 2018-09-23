3a02490b12695ac44c955c60be29bb9d943603f5
doomy <alexanderpaullozada@gmail.com>
2018-09-23 14:50:50 -0500

    dtolnay kindly suggested making the Post default.  Much better idea...

--- src/data/post.rs
+++ src/data/post.rs
@@ -7,74 +7,9 @@ enum PostState {
     Private,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "snake_case")]
-#[serde(tag = "type")]
-pub enum Post {
-    Text(Text),
-    Photo(Photo),
-    Quote(Quote),
-    Link(Link),
-    Answer(Answer),
-    Video(Video),
-    Audio(Audio),
-    Chat(Chat),
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Text {
-    #[serde(flatten)]
-    info: PostInfo,
-    title: Option<String>,
-    body: Option<String>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Quote {
-    #[serde(flatten)]
-    info: PostInfo,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Link {
-    #[serde(flatten)]
-    info: PostInfo,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Answer {
-    #[serde(flatten)]
-    info: PostInfo,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Video {
-    #[serde(flatten)]
-    info: PostInfo,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Audio {
-    #[serde(flatten)]
-    info: PostInfo,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Photo {
-    #[serde(flatten)]
-    info: PostInfo,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Chat {
-    #[serde(flatten)]
-    info: PostInfo,
-}
-
-
 /// All generic fields guarenteed to be present on every type of post
 #[derive(Serialize, Deserialize, Debug)]
-pub struct PostInfo {
+pub struct Post {
     id: usize,
     #[serde(rename="post_url")]
     url:				    String,
@@ -88,4 +23,60 @@ pub struct PostInfo {
     // liked:				bool,
     // state:				PostState,
     // total_posts:		u32
+    #[serde(flatten)]
+    content: Content,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "snake_case", tag = "type")]
+pub enum Content {
+    Text(Text),
+    Photo(Photo),
+    Quote(Quote),
+    Link(Link),
+    Answer(Answer),
+    Video(Video),
+    Audio(Audio),
+    Chat(Chat),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Text {
+    title: Option<String>,
+    body: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Quote {
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Link {
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Answer {
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Video {
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Audio {
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Photo {
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Chat {
+
 }
